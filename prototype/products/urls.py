afccde42ee4bc831d535c93d54e8e81f6c8d8c44
Hintro <58619451+Hintro@users.noreply.github.com>
2021-03-29 14:59:55 -0400

    integrated API

--- prototype/products/urls.py
+++ prototype/products/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'products'
 urlpatterns = [
     path('', views.search_product, name=None),
+    path('<results/', views.results, name='results'),
 ]
--- prototype/products/views.py
+++ prototype/products/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 
 def search_product(request):
     if request.method == 'POST':
-        return render(request, 'products/fetch_info.html', {'message': "You are using POST"})
+        product = request.POST['product_name']
+        return results(request, product)
     else:
         return render(request, 'products/fetch_info.html', {'message': "You are using GET"})
+
+
+def results(request, product):
+    return render(request, 'products/results.html', {'product': product})
