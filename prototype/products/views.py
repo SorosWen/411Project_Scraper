5de64c2662aeb5fea1fc3ba19d6b747b8aa40603
Hintro <58619451+Hintro@users.noreply.github.com>
2021-05-04 17:21:42 -0400

    Reformat views.py

--- prototype/products/views.py
+++ prototype/products/views.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
+from bs4 import BeautifulSoup
+import requests
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
+
+HEADERS = ({'User-Agent':
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
 
 
 def search_product(request):
@@ -12,96 +20,119 @@ def search_product(request):
         return render(request, 'products/fetch_info.html')
 
 
+############################################################################
+############################################################################
+# Amazon Scrapping Function #
+############################################################################
 """
-amazon_getInfo(url)
-Input: an url of a Amazon product page. 
-Funtion: Retrieve product name and price from the iput url. 
-Return value: a list with two strings: [product_name, price] 
+amazon_getSearchResult(product_name)
+Input: the name of a product. 
+Function: It take this product name and scrap information of all related products from Amazon. 
+Output: a list of product information, each element is the info of a product.
+	ex. [   [title1, price1, rating1], 
+			[title2, price2, rating2], 
+			[title3, price3, rating3]   ]
 """
-def amazon_getInfo(url):
-    s = HTMLSession()
-    r = s.get(url)
-    r.html.render(sleep=1)
-
-    # product name
-    try: 
-        product_name = r.html.xpath('//*[@id="productTitle"]', first=True).text
-    except: 
-        product_name = 'No name for display'
-    
-    # product price
-    try: 
-        product_price = r.html.xpath('//*[@id="priceblock_ourprice"]', first=True).text
-    except: 
-        product_price = 'No Price for display.'
-
-    # product rating
-    try: 
-        product_rating = r.html.xpath('//*[@id="acrCustomerReviewText"]', first=True).text.replace(' ratings', '')
-    except: 
-        product_rating = 'Rating is not available.'
-
-    product = [product_name, product_price, product_rating]
-    print(product)
-    return product
 
 
+def amazon_getSearchResult(product_name):
+    url = 'https://www.amazon.com/s?k=' + product_name.replace(' ', '+')
+    webpage = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(webpage.content, "lxml")
+    links = soup.find_all("a", attrs={'class': 'a-link-normal s-no-outline'})
+    product_list = []
+    count = 10
+    for link in links:
+        link = "https://www.amazon.com" + link.get('href')
+        product_list.append(amazon_product(link))
+        count -= 1
+        if count <= 0:
+            break
+    return product_list
 
-from django.shortcuts import  render, redirect
-from .forms import NewUserForm
-from django.contrib.auth import login
-from django.contrib import messages #import messages
+
+"""
+amazon_product(url) # helper
+Input: the url of the Amazon product page. 
+Output: a list of information about this product. [title, price, rating]
+"""
+
+
+def amazon_product(url):
+    webpage = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(webpage.content, "lxml")
+    title = soup.find("span", attrs={"id": 'productTitle'}).text.replace('\n', '').replace('\'', '')
+    price = get_price(soup)
+    rating = get_rating(soup)
+    return [title, price, rating]
+
+
+"""
+get_price(soup) # helper
+Input: a soup object of the Amazon product page. 
+Output: a string containing the price of this product. 
+"""
+
+
+def get_price(soup):
+    try:
+        price = soup.find("span", attrs={'id': 'priceblock_ourprice'}).string.strip()
+    except AttributeError:
+        price = ""
+    return price
+
+
+"""
+get_rating(soup) # helper
+Input: a soup object of the Amazon product page. 
+Output: a string containing the rating of this product. 
+"""
+
+
+def get_rating(soup):
+    try:
+        rating = soup.find("i", attrs={'class': 'a-icon a-icon-star a-star-4-5'}).string.strip()
+    except AttributeError:
+        try:
+            rating = soup.find("span", attrs={'class': 'a-icon-alt'}).string.strip()
+        except:
+            rating = ""
+    return rating
+
 
 def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("products:search")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm
-	return render (request=request, template_name="products/register.html", context={"register_form":form})
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("products:search")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm
+    return render(request=request, template_name="products/register.html", context={"register_form": form})
 
-
-
-
-from django.shortcuts import  render, redirect
-from .forms import NewUserForm
-from django.contrib.auth import login, authenticate #add this
-from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm #add this
 
 def login_request(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("products:search")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request=request, template_name="products/login.html", context={"login_form":form})
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("products:search")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="products/login.html", context={"login_form": form})
 
-
-
-
-
-from django.shortcuts import  render, redirect
-from .forms import NewUserForm
-from django.contrib.auth import login, authenticate, logout #add this
-from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
 
 def logout_request(request):
-	logout(request)
-	messages.info(request, "You have successfully logged out.") 
-	return redirect("products:search")
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("products:search")
